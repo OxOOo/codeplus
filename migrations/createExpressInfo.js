b7f63b9e0e1c9cffd18315c415318964c6dcd375
Liu Xiaoyi <circuitcoder0@gmail.com>
2017-11-24 16:24:21 +0900

    Fixed: wrong property id in migration script

--- migrations/createExpressInfo.js
+++ migrations/createExpressInfo.js
@@ -1,7 +1,7 @@
 db.users.find().snapshot().forEach(function(e) {
   db.users.update({ _id: e._id }, { $set: {
     express_info: {
-      addr: e.addr,
+      addr: e.address,
       school: e.school,
       receiver: e.real_name,
       phone: e.phone_number,
