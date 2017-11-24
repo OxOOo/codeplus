92f4ce561e8cf627253828df9202587d0e476ab7
Liu Xiaoyi <circuitcoder0@gmail.com>
2017-11-24 16:20:55 +0900

    Fixed another typo

--- migrations/createExpressInfo.js
+++ migrations/createExpressInfo.js
@@ -1,5 +1,5 @@
 db.users.find().snapshot().forEach(function(e) {
-  db.users.update({ _id: e._id, { $set: {
+  db.users.update({{ _id: e._id }, { $set: {
     express_info: {
       addr: e.addr,
       school: e.school,
